c2ce882e7eb8f77583832b431331c7df5c09d594
Future <beekayhuncho@gmail.com>
2022-09-04 17:04:52 +0200

    new database

--- store/settings.py
+++ store/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'store',
         'USER': 'postgres',
         'PASSWORD':'BokamosoBk@13',
