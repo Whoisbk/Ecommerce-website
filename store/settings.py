0c773f409005990c9a2ad44f04c38a5270eaa7ce
Future <beekayhuncho@gmail.com>
2022-09-08 15:49:03 +0200

    smtp removal

--- store/settings.py
+++ store/settings.py
@@ -154,4 +154,4 @@ EMAIL_HOST_PASSWORD = 'fzkabpxyvdosahjo'
 
 #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
